f534abb59e331bd7e8391adecb59de8b88b6a4b7
Sadık Uluçay <sadikulu45@gmail.com>
2024-06-27 14:32:32 +0300

    V25

--- src/main/java/com/meze/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meze.repository;
-
-import com.meze.domains.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment,Long> {
-}
